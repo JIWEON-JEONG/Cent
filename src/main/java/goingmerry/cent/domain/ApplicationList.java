f2e64cc14c6bddef69138363bdc04d815426c477
changeun <ypd05172@naver.com>
2022-05-18 23:27:41 +0900

    20220518 lcg 신청 목록 클래스, 리포지토리 생성

---
+++ src/main/java/goingmerry/cent/domain/ApplicationList.java
@@ -0,0 +1,15 @@
+package goingmerry.cent.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ApplicationList {
+
+    // 신청 목록
+
+   @Id
+    private String userEmail;
+
+
+}
---
+++ src/main/java/goingmerry/cent/repository/ApplicationListRepository.java
@@ -0,0 +1,9 @@
+package goingmerry.cent.repository;
+
+import goingmerry.cent.domain.ApplicationList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationListRepository extends JpaRepository<ApplicationList, String> {
+
+
+}
