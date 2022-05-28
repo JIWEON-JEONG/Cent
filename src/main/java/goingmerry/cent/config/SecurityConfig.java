f7b3bae92a6f6e5172ee4ef505da42d67f5f80ff
JIWEON-JEONG <dlektl6@naver.com>
2022-05-28 22:58:45 +0900

    [DELETE] OAUTH logic

--- src/main/java/goingmerry/cent/config/SecurityConfig.java
+++ src/main/java/goingmerry/cent/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package goingmerry.cent.config;
 
-import goingmerry.cent.oauth.OAuth2AccessTokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -15,7 +13,6 @@ import org.springframework.web.filter.CorsFilter;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final OAuth2AccessTokenAuthenticationFilter oAuth2AccessTokenAuthenticationFilter;
     private final CorsFilter corsFilter;
 
     @Override
@@ -34,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //              .anyRequest().hasRole("USER");
 
-        http.addFilterBefore(oAuth2AccessTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
 
